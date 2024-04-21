ed7bfced5dbd8b0d4119b3397f969def41e7e863
shree613 <rajasreedas0613@gmail.com>
2024-04-21 19:37:45 +0600

    Files Upload

---
+++ Emergency.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>EMERGENCY</title>
+    <link rel="stylesheet" href="Emergencystyle.css" />
+  </head>
+  <body>
+    <section class="header">
+        <nav class="navbar">
+          <a href="index.php"
+            ><img src="images/logo-header.png" alt="blooddrop logo" class="logo"
+          /></a>
+          <div class="mobile-nav">
+            <i href="#" class="fa fa-bars" style="color: white"></i>
+          </div>
+          <div class="nav-links">
+            <ul>
+              <li><a href="index.php">Home</a></li>
+              <li><a  onClick="document.getElementById('whywe').scrollIntoView();">Why we?</a></li>
+              <li><a href="">About us</a></li>
+              <a class='loginbtn' onclick="document.getElementById('login-form').style.display='block'" style="width:auto;">Login</a>
+              <li class="emrg"><a href="Emergency.html"> EMERGENCY! </a></li>
+            </ul>
+          </div>
+        </nav>
+    <div class="container">
+      <div class="card">
+        <div class="box">
+          <div class="content">
+            <h2>HOSPITAL NAME</h2>
+            <br />
+            <p>
+              National hospital<br />
+              14/15, Chattogram 4000
+            </p>
+            <br />
+            <p>
+              United hospital limited<br />
+              Plot # 15 Rd no. 71, Dhaka 1212
+            </p>
+            <br />
+            <p>
+              Oasis hospital<br />
+              Subhanighat, bishwa Rd, Sylhet 3100
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="card">
+        <div class="box">
+          <div class="content">
+            <h2>AMBULANCE NUMBER</h2>
+            <br />
+            <p>CTG--01933-246577</p>
+            <br />
+            <p>DHA--01720-448666</p>
+            <br />
+            <p>SYL--01713-205555</p>
+          </div>
+        </div>
+      </div>
+      <div class="card">
+        <div class="box">
+          <div class="content">
+            <h2>BLOOD BANK</h2>
+            <br />
+            <p>
+              Chittagong Blood Bank & Transfusion Centre<br />
+              Mayabini building 57 K.B. Fazlul Khader RD, east Gate, Chattogram
+            </p>
+            <br />
+            <p>
+              Blood Bank - Bangladesh Red Crescent Society<br />
+              7, 5 Aurangajeb Rd, Dhaka
+            </p>
+            <br />
+            <p>
+              Cresent Blood Bank <br />
+              Dorgah Moholla Rd, Sylhet 3100
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
